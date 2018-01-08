5b51ba636248e01b94729255337e7cbe884c61b3
yuqinglove189 <32956274+yuqinglove189@users.noreply.github.com>
2018-01-08 11:18:38 +0800

    2

--- main/main.js
+++ main/main.js
@@ -1,8 +1,8 @@
 module.exports = function printInventory(inputs) {
-    var barcode_list=get_barcode_list(inputs);
-    var shopping_cart=get_shopping_cart(barcode_list);
-    var list_print=get_shopping_lists(shopping_cart);
-    console.log(list_print);
+    var barcodeList=getBarcodeList(inputs);
+    var shoppingCart=getShoppingCart(barcodeList);
+    var listPrint=getShoppingLists(shoppingCart);
+    console.log(listPrint);
 };
 
 function loadAllItems() {
@@ -59,7 +59,7 @@ function loadPromotions() {
     ];
 }
 
-function get_barcode_list(inputs){
+function getBarcodeList(inputs){
     var barcodes={};
     inputs.forEach(function(input){
         if(barcodes[input]||input.indexOf('-')!=-1){
@@ -71,12 +71,12 @@ function get_barcode_list(inputs){
     });
     return barcodes
 }
-function get_shopping_cart(barcode_list){
+function getShoppingCart(barcodeList){
     var items=loadAllItems();
     var shopping_cart=[];
     items.forEach(function(item){
-        if(barcode_list[item.barcode]){
-            item.count=barcode_list[item.barcode];
+        if(barcodeList[item.barcode]){
+            item.count=barcodeList[item.barcode];
             item.free=Math.floor(item.count/3);
             shopping_cart.push(item);
         }
@@ -84,12 +84,12 @@ function get_shopping_cart(barcode_list){
 
     return shopping_cart
 }
-function get_shopping_lists(shopping_cart){
+function getShoppingLists(shoppingCart){
     var list='***<没钱赚商店>购物清单***';
     var list_free='----------------------'+'\n'+'挥泪赠送商品：';
     var sum=0;
     var save=0;
-    shopping_cart.forEach(function(lists){
+    shoppingCart.forEach(function(lists){
         var subtotal=(lists.count-lists.free)*lists.price
         list=list+'\n'+'名称：'+lists.name+',数量：'+lists.count+lists.unit+',单价：'+lists.price.toFixed(2)+'(元),小计：'+subtotal.toFixed(2)+'(元)'
         sum+=subtotal;
